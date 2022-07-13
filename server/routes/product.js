193280444ac13da1b09b8f158711c04d5fd95b09
happy8131 <lion8131@naver.com>
2022-07-13 13:10:07 +0900

    [Add] product save database

--- server/routes/product.js
+++ server/routes/product.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const multer = require("multer");
-
+const { Product } = require("../models/Product");
 //=================================
 //             Product
 //=================================
@@ -36,6 +36,30 @@ router.post("/image", (req, res) => {
       fileName: res.req.file.filename,
     });
   });
+});
+
+router.post("/", (req, res) => {
+  //받아온 정보들을 디비에 넣어 준다.
+  const product = new Product(req.body);
+
+  product.save((err) => {
+    if (err) return res.status(400).json({ success: false, err });
+    return res.status(200).json({ success: true });
+  });
+});
+
+router.post("/products", (req, res) => {
+  //product collection에 들어 있는 모든 상품 정보를 가져오기
+
+  Product.find({})
+    .populate("writer")
+    .exec((err, productInfo) => {
+      if (err) return res.status(400).json({ success: false, err });
+      return res.status(200).json({
+        success: true,
+        productInfo,
+      });
+    });
 });
 
 module.exports = router;
